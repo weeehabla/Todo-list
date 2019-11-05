777ece96fc7df14fb0ead254aa384d10089b2207
abdelsalamshahlol <abdulsalamshahlol@gmail.com>
2019-11-05 11:43:03 +0100

    Revert "basic features complete"
    This reverts commit 97ece2d3e9a6d1d22686d4c8044df8de2439800e.
    
    removed wrong contributer from repo

--- js/main.js
+++ js/main.js
@@ -35,29 +35,6 @@ $(document).ready(function() {
 		updateDom();
 	});
 
-	// Handel Delete Task 
-	$("body").on("click", '.task-remove-btn', function() {
-		var src = $(this);
-		var listId = parseInt(src.attr('data-list-id'));
-		var taskId = parseInt(src.attr('data-task-id'));
-
-		removeTask(listId, taskId);
-		updateDom();
-	});
-	// Handle Compelte Task.
-
-	$("body").on("click", ".task-complete-btn", function() {
-		var src = $(this);
-		var listId = parseInt(src.attr('data-list-id'));
-		var taskId = parseInt(src.attr('data-task-id'));
-		completeTask(listId, taskId);
-		updateDom();
-
-
-	})
-
-
-
 	function saveTask() {
 		var list = $('#list-select');
 		var listName = list.val();
@@ -66,9 +43,7 @@ $(document).ready(function() {
  		});
 		console.log(index)
 		if(index >= 0){
-			var input = $("#create-task-input");
-			var content = input.val();
-			input.val("");
+			var content = $("#create-task-input").val();
 			var task = Task(content);
 			listCollec[index].addTask(task);
 		}else{
@@ -76,55 +51,42 @@ $(document).ready(function() {
 		}
 	}
 
-	function removeTask(listId, taskId) {
-		var task = listCollec[listId].getTask(taskId);
-		console.log(task);
-
-		listCollec[listId].removeTask(task);
-		alert('task removed');
-	}
-
-	function completeTask(listId, taskId) {
-		var task = listCollec[listId].getTask(taskId);
-		console.log(task)
-		task.toggleStatus()
-		console.log(task) 
-
-	}
-
 	function updateDom() {
 		
-		for(var i in listCollec) {
+		for(var list of listCollec) {
+			// var row = $("<div class='row'>");
+			// var col = $("<div class='col'>");
+			// col.append
 			var html = `<div class="row">
 			<div class="col">
-				<h4>${listCollec[i].name}</h4>`
-				for(var task of listCollec[i].tasks) {
-					var content = task.isComplete ? `<s>${task.content}</s>`: task.content
+				<h4>${list.name}</h4>`
+				for(var task of list.task) {
+					
 					html +=`<div class="task-wrap">
 					<div class="row">
 						<div class="col">
-							<span>${content}</span>
+							<span>${task.content}</span>
 						</div>
 						<div class="col">
+							<button>Complete</button>
 							<small>Date</small>
-							<button class="task-remove-btn"
-							 data-list-id="${i}" 
-							 data-task-id="${task.id}">Remove</button>
-							<button class="task-complete-btn" data-task-id="${task.id}"
-							data-list-id="${i}">Complete</button>
 						</div>
 					</div>
 				</div>`
 
-				}	
+				}
+				
 			html +=`
 			</div>
 		</div>	`
 		$("#show").html(html)
-		}
-	}
 
-	// Storage Function 
+
+		}
+
+
+
+	}
 });
 
 	
